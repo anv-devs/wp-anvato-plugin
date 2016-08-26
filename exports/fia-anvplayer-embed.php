d616d8a34671fb8ab77a53bb581431b7a027eece
TinkerWorkshop <eugene.grybenko@gmail.com>
2016-08-26 13:13:19 -0400

    Add Facebook INstant Articles file for handling AnvPlayer shortcode

---
+++ exports/fia-anvplayer-embed.php
@@ -0,0 +1,41 @@
+<?php
+
+// deregister original shortcode
+remove_shortcode( 'anvplayer' );
+// add new facebook shortcode for anvplayer
+add_shortcode( 'anvplayer', function( $args ) {
+
+	$parameters = anvato_shortcode_get_parameters( $attr );
+
+	$iframe_src = 'https://w3.cdn.anvato.net/player/prod/anvload.html?key=' . 
+		base64_encode( json_encode( $parameters['json'] ) );
+
+	$iframe_width = 640;
+	if ( !empty( $parameters['player']['width'] ) && 'px' === $parameters['player']['width_type'] ) {
+		$iframe_width = $parameters['player']['width'];
+	}
+	$iframe_height = 360;
+	if ( !empty( $parameters['player']['height'] ) && 'px' === $parameters['player']['height_type'] ) {
+		$iframe_height = $parameters['player']['height'];
+	}
+
+	// Construct the element
+	$iframe_html =
+		'<iframe ' .
+			'src="' . esc_url( $iframe_src ) . '" ' .
+			'width="' . esc_attr( $iframe_width ) . '" height="' . esc_attr( $iframe_height ) . '" ' . 
+			'sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" ' .
+			'allowfullscreen ' .
+			'layout="responsive" ' .
+			'scrolling="no" ' .
+			'frameborder="0" ' .
+		'></iframe>';
+
+	/*
+		Please note that adding "figure" tag deleted the shortcode processing.
+		Escaming will be done automatically and should not be included.
+	*/
+
+	return $iframe_html;
+
+} );
